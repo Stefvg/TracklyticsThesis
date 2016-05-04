b4249702ed528581ff0f216748545d8eae600282
Stefvg <stefvangils@MacBook-Pro-van-Stef.local>
2016-05-04 16:40:14 +0200

    Save On Disk or not

--- Backend + Dashboard/js/settingsController.js
+++ Backend + Dashboard/js/settingsController.js
@@ -9,6 +9,7 @@
         var settings = this;
         settings.enabled = 1;
         settings.aggregation = 0;
+        settings.disk = 0;
 
         var app = location.search.split('app=')[1];
         app = decodeURIComponent(app);
@@ -19,7 +20,9 @@
         $http.get('../php/ShouldAggregateOnDevice.php?app=' + app).success(function(data){
             settings.aggregation = parseInt(data);
         });
-
+        $http.get('../php/ShouldSaveOnDisk.php?app=' + app).success(function(data){
+            settings.disk = parseInt(data);
+        });
         $http.get('../php/getAppCode.php?app=' + app).success(function(data){
             $scope.appCode = data;
         });
@@ -36,6 +39,10 @@
             $http.get('../php/ChangeAggregationOnDevice.php?app=' + app +'&value=' + value).success(function(data){
             });
         };
-
+        $scope.changeSaveOnDisk = function() {
+            var value = settings.disk ? 1 : 0;
+            $http.get('../php/ChangeSaveOnDisk.php?app=' + app +'&value=' + value).success(function(data){
+            });
+        };
     }]);
 })(window.angular);
